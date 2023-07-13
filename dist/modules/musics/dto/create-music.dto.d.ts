1f56bb9c5eb20ed6befb7322a1029e3345752917
Leonaro-L-Cunha <leonavidareal@gmail.com>
2023-07-13 01:33:31 -0300

    feat:fix

--- dist/modules/musics/dto/create-music.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateMusicDto {
-    name: string;
-    album: string;
-    artist: string;
-    genre: string;
-    year: string;
-    cover_image: string;
-    music_url: string;
-}
--- dist/modules/musics/repository/music.repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MusicRepository = void 0;
-class MusicRepository {
-}
-exports.MusicRepository = MusicRepository;
-//# sourceMappingURL=music.repository.js.map
