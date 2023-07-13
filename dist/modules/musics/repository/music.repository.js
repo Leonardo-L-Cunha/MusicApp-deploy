752f191db231a75fe396322d4d52d872a7f62f38
Leonaro-L-Cunha <leonavidareal@gmail.com>
2023-07-13 01:26:13 -0300

    fix:deploy

---
+++ dist/modules/musics/repository/music.repository.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MusicRepository = void 0;
+class MusicRepository {
+}
+exports.MusicRepository = MusicRepository;
+//# sourceMappingURL=music.repository.js.map
